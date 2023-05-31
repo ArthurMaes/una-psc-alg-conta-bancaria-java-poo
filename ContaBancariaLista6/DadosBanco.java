4b2e5bb4b85d8ad11c892eb2e3c70b4cb3a20682
Arthur Maestri <40868046+ArthurMaes@users.noreply.github.com>
2023-05-31 20:15:13 -0300

    Add files via upload

---
+++ ContaBancariaLista6/DadosBanco.java
@@ -0,0 +1,24 @@
+public class DadosBanco {
+    private String nome;
+    private int numeroConta;
+    private double saldoInicial;
+    
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    public void setNumeroConta(int numeroConta) {
+        this.numeroConta = numeroConta;
+    }
+    public double getSaldoInicial() {
+        return saldoInicial;
+    }
+    public void setSaldoInicial(double saldoInicial) {
+        this.saldoInicial = saldoInicial;
+    }
+}
